8ca01d5a4d154b183f85b24fb195d024906b5dda
wenqingyu <yuwenqingisu@gmail.com>
2018-10-16 14:25:26 +0800

    change config into env

--- sendTest.js
+++ sendTest.js
@@ -1,11 +1,13 @@
 const Eos = require('eosjs')
+require('dotenv').config()
+
 // const EosAPI = require('eosjs-api')
 
 // Default configuration
 const config = {
-  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906', // 32 byte (64 char) hex string
-  keyProvider: [''], // WIF string or array of keys..
-  httpEndpoint: 'https://api1.eosasia.one',
+  chainId: process.env.CHAIN_ID, // 32 byte (64 char) hex string
+  keyProvider: [process.env.PRIVATE_KEY], // WIF string or array of keys..
+  httpEndpoint: process.env.HTTP_ENDPOINT,
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
@@ -16,9 +18,9 @@ const eos = Eos(config)
 // const eosReadOnly = EosAPI(config)
 
 const options = {
-  authorization: 'wenqingyu222@active',
+  authorization: 'fromUserID@active',
   broadcast: true,
   sign: true
 }
 
-eos.transfer('wenqingyu222', 'dapprofessor', '0.2500 EOS', 'this is a 中文测试', options)
+eos.transfer('fromUserID', 'toUserID', '0.2500 EOS', 'this is MEMO', options)

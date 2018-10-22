216cdb627d6e5982ec600a7742c6fb501bf8d696
wenqingyu <yuwenqingisu@gmail.com>
2018-10-22 14:59:07 +0800

    add getAccountInfo

---
+++ getAccountInfo.js
@@ -0,0 +1,41 @@
+/**
+ * Get EOS Account Info
+ */
+
+const Eos = require('eosjs')
+require('dotenv').config()
+const EosAPI = require('eosjs-api')
+
+// Default configuration
+const config = {
+  chainId: process.env.CHAIN_ID, // 32 byte (64 char) hex string
+  keyProvider: [process.env.PRIVATE_KEY], // WIF string or array of keys..
+  httpEndpoint: process.env.HTTP_ENDPOINT,
+  expireInSeconds: 60,
+  broadcast: true,
+  verbose: false, // API activity
+  sign: true
+}
+
+const eos = Eos(config)
+const eosReadOnly = EosAPI(config)
+
+// const options = {
+//   authorization: 'fromUserID@active',
+//   broadcast: true,
+//   sign: true
+// }
+
+// eos.transfer('fromUserID', 'toUserID', '0.2500 EOS', 'this is MEMO', options)
+
+async function main () {
+  let history = await eos.getActions('EOSAccount', 1, 100) // MAX Offset 100
+  let actions = history.actions
+  let output = actions.map(a => a.actions_trace)
+  console.log(actions)
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
